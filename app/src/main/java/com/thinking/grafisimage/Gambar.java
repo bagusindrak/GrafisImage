54ffdc3f05f09cfc8d926014ceb0ebc03699ec9e
bagusindrak <indra22071999@gmail.com>
2019-11-01 20:52:52 +0700

    xml gambar

---
+++ app/src/main/java/com/thinking/grafisimage/Gambar.java
@@ -0,0 +1,18 @@
+package com.thinking.grafisimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Gambar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gambar);
+    }
+
+    public void tampil(View view) {
+    }
+}
