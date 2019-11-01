63304b902e53954a172f28bd76420fb164c38e8a
bagusindrak <indra22071999@gmail.com>
2019-11-01 19:32:14 +0700

    Segitiga

--- app/src/main/java/com/thinking/grafisimage/GrafikKlasik.java
+++ app/src/main/java/com/thinking/grafisimage/GrafikKlasik.java
@@ -2,8 +2,10 @@ package com.thinking.grafisimage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.Button;
 
 public class GrafikKlasik extends AppCompatActivity {
@@ -15,7 +17,32 @@ public class GrafikKlasik extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grafik_klasik);
         tekan = (Button) findViewById(R.id.button1);
-        grafik1 = new GLSurfaceView(this);
+        grafik1 = new HelloESSurfaceView(this);
         setContentView(grafik1);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        grafik1.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        grafik1.onResume();
+    }
+
+    class HelloESSurfaceView extends GLSurfaceView{
+        public HelloESSurfaceView(Context context){
+            super(context);
+            setRenderer(new Segitiga());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
